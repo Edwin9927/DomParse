7e225dcf40bae7f8eef01372fad0c9ca604efcee
Edwin9927 <edwinmanzanoquinzo@gmail.com>
2021-01-05 20:16:07 -0500

    Agregar archivos de proyecto.

---
+++ DomParse/Actividad14_6621_6626.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace DomParse
+{
+    class Actividad14_6621_6626
+    {
+        private const String filename = "../../../Store.xml";//Path del archivo XML
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Actividad 14\n\tEdwin Manzano   6626\n\tEstalin Morocho 6621\n");
+            DomParser objDom = new DomParser();
+            objDom.leer(filename);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ DomParse/DomParser.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace DomParse
+{
+    class DomParser
+    {
+        public void leer(string filename)
+        {
+
+            try
+            {
+                XmlDocument Doc = new XmlDocument();
+                Doc.Load(filename);
+
+                XmlNodeList raiz = Doc.DocumentElement.SelectNodes("Juego");
+                Console.WriteLine("Lista de juegos: \n");
+                foreach (XmlNode nodo in raiz)
+                {
+                    Console.WriteLine("Nombre: " + nodo.ChildNodes.Item(0).InnerText);
+                    Console.WriteLine("Genero: " + nodo.ChildNodes.Item(1).InnerText);
+                    Console.WriteLine("Fecha: " + nodo.ChildNodes.Item(2).InnerText);
+                    Console.WriteLine("Desarrollador:" + nodo.ChildNodes.Item(3).InnerText);
+                    Console.WriteLine("Precio:" + nodo.ChildNodes.Item(4).InnerText);
+                    Console.WriteLine("Unidades:" + nodo.ChildNodes.Item(5).InnerText);
+                    Console.WriteLine("\n");
+                }
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.WriteLine("Juegos con poco stock: \n");
+
+                foreach (XmlNode nodo in raiz)
+                {
+                    if(int.Parse(nodo.ChildNodes.Item(5).InnerText) < 4)
+                    {
+                        Console.WriteLine("Nombre: " + nodo.ChildNodes.Item(0).InnerText);
+                        Console.WriteLine("Genero: " + nodo.ChildNodes.Item(1).InnerText);
+                        Console.WriteLine("Fecha: " + nodo.ChildNodes.Item(2).InnerText);
+                        Console.WriteLine("Desarrollador:" + nodo.ChildNodes.Item(3).InnerText);
+                        Console.WriteLine("Precio:" + nodo.ChildNodes.Item(4).InnerText);
+                        Console.WriteLine("Unidades:" + nodo.ChildNodes.Item(5).InnerText);
+                        Console.WriteLine("\n");
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
